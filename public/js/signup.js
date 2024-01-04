0af4ee65698090eeb72ae511b970acca8e6e3607
Brian12345-webdev <132722257+Brian12345-webdev@users.noreply.github.com>
2024-01-03 18:57:59 -0600

    update, completed signup.js file

---
+++ public/js/signup.js
@@ -0,0 +1,27 @@
+const signupHandler = async (event) => {
+    event.preventDefault();
+
+    const username = document.querySelector('#username').value.trim();
+    const password = document.querySelector('#password').value.trim();
+    const email = document.querySelector('#email').value.trim();
+
+    if (username && email && password) {
+        const response = await fetch('api/users/signup', {
+            method: 'POST',
+            body: JSON.stringify({ username, email, password }),
+            headers: { 'Content-Type': 'application/json'},
+
+        });
+
+        if (response.ok) {
+            document.location.replace('/');
+        }else {
+            alert('Could not complete signup! Please try again.');
+        }
+    }
+};
+
+const signupListner = document.querySelector('#signup');
+if (signupListner) {
+    signupListner.addEventListener('submit', signupHandler);
+}

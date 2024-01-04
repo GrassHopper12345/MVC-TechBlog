5b8ba6ba7a4f4c1b1898d697a464ecddfca99419
Brian12345-webdev <132722257+Brian12345-webdev@users.noreply.github.com>
2024-01-03 18:24:40 -0600

    update, new_post.js complete

---
+++ public/js/new_post.js
@@ -0,0 +1,26 @@
+const newPostHandler = async (event) => {
+    event.preventDefault();
+
+    const title = document.querySelector('#title-new-post').value.trim();
+
+    const body = document.querySelector('#body-new-post').value.trim();
+
+    if (title && body) {
+        const response = await fetch('/api/posts', {
+            method: 'POST',
+            body: JSON.stringify({ title, body}),
+            headers: {'Content-Type': 'application/json'},
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        }else{
+            alert('Error, failed to create new post! Please try again');
+        }
+    }
+};
+
+const newPost = document.querySelector('#new-post');
+if (newPost) {
+    newPost.addEventListener('submit', newPostHandler);
+}

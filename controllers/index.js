cd2f1375c3cb84616845bdf34196bfa3cac9f5d0
Brian12345-webdev <132722257+Brian12345-webdev@users.noreply.github.com>
2024-01-06 10:43:12 -0600

    update, completed index.js routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,10 @@
 const router = require('express').Router();
-const userRoutes = require('./user_routes');
-const postRoutes = require('./post_routes');
-const commentRoutes = require('comment_routes');
+const apiRoutes = require('./api');
+const homeRoutes = require('./home_routes');
 
-router.use('/user_routes', userRoutes);
-router.use('/comment_routes', commentRoutes);
-router.use('/post_routes', postRoutes);
+router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
+
 
 
 module.exports = router;

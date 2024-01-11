4e758c1380756d7bf771155471f99ea174e97732
Brian12345-webdev <132722257+Brian12345-webdev@users.noreply.github.com>
2024-01-10 21:11:12 -0600

    update to helper

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,6 +1,12 @@
 module.exports = {
     format_date: (date) => {
       // Format date as MM/DD/YYYY
-      return date.toLocaleDateString();
+      const date = new Date(date);
+      const formatDate = `${date.getMonth()+1}/${date.getDate()}/${date.getFullYear()}`;
+      const hour = date.getHours().toString().padStart(2, '0');
+      const minute = date.getMinutes().toString().padStart(2, '0');
+      const second = date.getSeconds().toString().padStart(2, '0');
+      const formatTime = `${hour}:${minute}:${second}`;
+      return `${formatDate} at ${formatTime}`;
     },
   };

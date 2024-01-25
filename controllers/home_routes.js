61782ceaa28221a3f89014c195fa3aa9a208f09f
Brian12345-webdev <132722257+Brian12345-webdev@users.noreply.github.com>
2024-01-24 21:08:02 -0600

    update to models

--- controllers/home_routes.js
+++ controllers/home_routes.js
@@ -8,7 +8,7 @@ router.get('/', async (req, res) => {
       include: [{ model: User, attributes: ['username'] }],
     });
     const posts = postData.map((post) => post.get({ plain: true }));
-
+    console.log(posts);
     res.render('homepage', {
       posts,
       logged_in: req.session.logged_in,

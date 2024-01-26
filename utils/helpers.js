befcbadbce90b2be31dee76dd5ad2eafa684b594
Brian12345-webdev <132722257+Brian12345-webdev@users.noreply.github.com>
2024-01-25 21:27:20 -0600

    removed the format time and created_at to see if that is whats effecting the deployed site

--- utils/helpers.js
+++ utils/helpers.js
@@ -4,11 +4,11 @@ module.exports = {
     // Format date as MM/DD/YYYY
     const dates = new Date(date);
     const formatDate = `${dates.getMonth() + 1}/${dates.getDate()}/${dates.getFullYear()}`;
-    const hour = dates.getHours().toString().padStart(2, '0');
-    const minute = dates.getMinutes().toString().padStart(2, '0');
-    const formatTime = `${hour}:${minute}`;
+    // const hour = dates.getHours().toString().padStart(2, '0');
+    // const minute = dates.getMinutes().toString().padStart(2, '0');
+    // const formatTime = `${hour}:${minute}`;
     console.log(formatDate);
     console.log(formatTime);
-    return `${formatDate} at ${formatTime}`;
+    return `${formatDate}`;
   },
 };

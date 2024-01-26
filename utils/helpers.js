d5b3da6303460a47cb2326f630d72f203d0a60c4
Brian12345-webdev <132722257+Brian12345-webdev@users.noreply.github.com>
2024-01-25 21:32:59 -0600

    tried revamping helper.js and added date_created to required handlebars

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,14 +1,26 @@
 module.exports = {
   format_date: (date) => {
-    console.log(date);
     // Format date as MM/DD/YYYY
-    const dates = new Date(date);
-    const formatDate = `${dates.getMonth() + 1}/${dates.getDate()}/${dates.getFullYear()}`;
-    // const hour = dates.getHours().toString().padStart(2, '0');
-    // const minute = dates.getMinutes().toString().padStart(2, '0');
-    // const formatTime = `${hour}:${minute}`;
-    console.log(formatDate);
-    console.log(formatTime);
-    return `${formatDate}`;
+    return date.toLocaleDateString();
   },
 };
+
+
+
+
+
+
+// module.exports = {
+//   format_date: (date) => {
+//     console.log(date);
+//     Format date as MM/DD/YYYY
+//     const dates = new Date(date);
+//     const formatDate = `${dates.getMonth() + 1}/${dates.getDate()}/${dates.getFullYear()}`;
+//     const hour = dates.getHours().toString().padStart(2, '0');
+//     const minute = dates.getMinutes().toString().padStart(2, '0');
+//     const formatTime = `${hour}:${minute}`;
+//     console.log(formatDate);
+//     console.log(formatTime);
+//     return `${formatDate}`;
+//   },
+// };
